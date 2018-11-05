5faf40c9e09a20a61d03f1d31962f5869322a47c
Felipe Getúlio <felipegetulio15@gmail.com>
2018-11-05 14:32:52 -0300

    Generating all partitions - Finished

---
+++ practical-project-4/combination.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  5 12:48:50 2018
+
+@author: FelipeLaranjeira
+"""
+
+def partitions(n):
+    assert isinstance(n, int), 'n must be an integer'
+    assert n > 0, 'n must be a natural number but zero.'
+    
+    a = list(range(n+1))
+    tuples = []
+    
+    for m in range(2, n+1):
+        a[m] = 1
+    
+    m = 1
+    a[0] = 0
+    
+    while(True):
+        
+        a[m] = n
+        q = m - int(n == 1)
+        
+        while(True):
+            tuples.append(tuple(a[1:m+1]))
+            
+            if(a[q] != 2):
+                break
+            
+            a[q] = 1
+            q -= 1
+            m += 1
+      
+        if(q == 0):
+            break
+        
+        x = a[q] - 1
+        a[q] = x
+        n = m - q + 1
+        m = q + 1
+        
+        while(n > x):
+            
+            a[m] = x
+            m += 1
+            n -= x
+            
+    return tuples
+
+
+tuples = partitions(4)
+print(tuples)
